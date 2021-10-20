20fdf3d7f28bb8367f8d02c7b0c6ef90fc5c1706
Julien Champagnol <julien.champagnol@geode-solutions.com>
2021-10-20 13:00:39 +0200

    Delete upload directory/create it with python

--- app.py
+++ app.py
@@ -88,10 +88,12 @@ def ListExtensions(ObjectsList):
     return List # Returns the final list
 
 if __name__ == '__main__':
-    # print("coucou")
+    
+    if not os.path.exists("./uploads"):
+        os.mkdir("./uploads")
+    
     app.run(debug=True, host='0.0.0.0', port=5000)#If main run in debug mode
     flask_cors.CORS(app)  # This makes the CORS feature cover all routes in the app
     ObjectsList = GeodeObjects.ObjectsList()
     print(ListExtensions(ObjectsList))
-    # print(ListObjects(ObjectsList, "og_brep"))
-    # test.testfunction()
+
