49936cdd627f481d22bed7c5616999580fc050ca
Julien Champagnol <julien.champagnol@geode-solutions.com>
2021-11-18 16:00:24 +0100

    port 443

--- app.py
+++ app.py
@@ -135,11 +135,13 @@ def ListExtensions(ObjectsList):
     return List  # Returns the final list
 
 
+443
+
 if __name__ == '__main__':
     if not os.path.exists("./uploads"):
         os.mkdir("./uploads")
 
     # set_interval(update_or_kill, False, 30)
 
-    app.run(debug=True, host='0.0.0.0', port=5000,
+    app.run(debug=True, host='0.0.0.0', port=443,
             threaded=False, ssl_context='adhoc')  # , ssl_context='adhoc'
