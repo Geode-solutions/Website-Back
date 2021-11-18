b9b8d78c96135533cb91cf5d2ed47f454f25007a
Julien Champagnol <julien.champagnol@geode-solutions.com>
2021-11-18 11:13:17 +0100

    test ssl_context

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ if __name__ == '__main__':
     # set_interval(update_or_kill, False, 30)
 
     app.run(debug=True, host='0.0.0.0', port=5000,
-            threaded=False, ssl_context='adhoc')
+            threaded=False)  # , ssl_context='adhoc'
