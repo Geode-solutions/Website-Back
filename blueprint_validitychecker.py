bbdd26f68b390ad9bfcc457ebe27fb7b71e7b1a9
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-02-20 11:33:08 +0100

    🔧 fix tuple loop CME.string() 🔧 fix components_nb_degenerated_elements 📌 update requirements

--- blueprint_validitychecker.py
+++ blueprint_validitychecker.py
@@ -82,6 +82,7 @@ def vaditychecker_inspectfile():
         filename = flask.request.form.get('filename')
         test = flask.request.form.get('test')
 
+        print(f'{test=}', flush=True)
         if object is None:
             return flask.make_response({"error_message": "No object sent"}, 400)
         if filename is None:
@@ -99,16 +100,23 @@ def vaditychecker_inspectfile():
         expectedValue = InspectorFunctions.InpectorExpectedResults()[test]
 
         if testResult != expectedValue or type(testResult) != type(expectedValue):
-            print(test, ' : ', testResult)
-            if type(testResult) is dict:
-                for key in testResult:
-                    new_key = key.string()
-                    testResult[new_key] = testResult.pop(key)
-
+            if type(testResult) is list:
+                if type(testResult[0]) is tuple:
+                    temp_testResult = []
+                    for tuple_item in testResult:
+                        temp_list = []
+                        for index in range(len(tuple_item)):
+                            temp_list.append(tuple_item[index].string())
+                        temp_testResult.append(temp_list)
+                    testResult = temp_testResult
         result = testResult == expectedValue and type(testResult) == type(expectedValue)
+
+        if (result == False):
+            print(f'{test=}', flush=True)
 
         return flask.make_response({"Result": result, "list_invalidities": str(testResult)}, 200)
 
     except Exception as e:
-        print("error : ", str(e))
+        print("error : ", str(e), flush=True)
+        print(f'{test=}', flush=True)
         return flask.make_response({"error_message": str(e)}, 500)
