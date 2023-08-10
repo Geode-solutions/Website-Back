52ec6765845fe00b837039f82b94773d4ca492a4
Clemamolette <clementinegaga79@outlook.fr>
2023-08-10 14:16:25 +0200

    fix : loading data

--- blueprints/tools/blueprint_file_converter.py
+++ blueprints/tools/blueprint_file_converter.py
@@ -86,7 +86,7 @@ async def file_converter_convert_file():
 
     secure_filename = werkzeug.utils.secure_filename(filename)
     file_path = os.path.join(UPLOAD_FOLDER, secure_filename)
-    data = geode_functions.geode_objects.objects_list()[geode_object]['load'](file_path)
+    data = geode_functions.load(geode_object, file_path)
     strict_file_name = os.path.splitext(secure_filename)[0]
     new_file_name = strict_file_name + '.' + extension
 
