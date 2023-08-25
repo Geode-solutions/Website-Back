7c074e33a46784f10053077bc66529e65ac4efbd
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-08-25 14:17:03 +0200

    remove ID in tests

--- tests/test_explicit.py
+++ tests/test_explicit.py
@@ -1,17 +1,16 @@
 import os
 import base64
 
-ID = os.environ.get('ID')
-base_route = f"/{ID}/workflows/explicit"
+base_route = f"/workflows/explicit"
 
 
 def test_get_brep_stats(client):
-    response = client.post(f'{base_route}/get_brep_stats')
+    response = client.post(f"{base_route}/get_brep_stats")
     assert response.status_code == 200
-    nb_corners = response.json['nb_corners']
-    nb_lines = response.json['nb_lines']
-    nb_surfaces = response.json['nb_surfaces']
-    nb_blocks = response.json['nb_blocks']
+    nb_corners = response.json["nb_corners"]
+    nb_lines = response.json["nb_lines"]
+    nb_surfaces = response.json["nb_surfaces"]
+    nb_blocks = response.json["nb_blocks"]
     assert type(nb_corners) is int
     assert type(nb_lines) is int
     assert type(nb_surfaces) is int
