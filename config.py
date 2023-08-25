8a531e8b677c0abfc5854152d90af381f2a64968
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-08-25 13:36:23 +0200

    Update port 5000

--- config.py
+++ config.py
@@ -1,25 +1,28 @@
-''' Flask configuration '''
+""" Flask configuration """
 import os
 
+
 class Config(object):
-    FLASK_DEBUG = os.environ.get('FLASK_DEBUG', default=False)
-    ID = os.environ.get('ID', default=None)
-    PORT = '443'
-    CORS_HEADERS = 'Content-Type'
-    UPLOAD_FOLDER = './uploads'
-    DATA_FOLDER = '/data/'
-    WORKFLOWS_DATA_FOLDER = './data_workflows/'
-    LOCK_FOLDER = './lock'
-    TIME_FOLDER = './time'
+    FLASK_DEBUG = os.environ.get("FLASK_DEBUG", default=False)
+    ID = os.environ.get("ID", default=None)
+    PORT = "5000"
+    CORS_HEADERS = "Content-Type"
+    UPLOAD_FOLDER = "./uploads"
+    DATA_FOLDER = "/data/"
+    WORKFLOWS_DATA_FOLDER = "./data_workflows/"
+    LOCK_FOLDER = "./lock"
+    TIME_FOLDER = "./time"
+
 
 class ProdConfig(Config):
-    SSL = 'adhoc'
-    ORIGINS = ['https://geode-solutions.com', 'https://next.geode-solutions.com']
-    MINUTES_BEFORE_TIMEOUT = '5'
-    SECONDS_BETWEEN_SHUTDOWNS = '45'
+    SSL = "adhoc"
+    ORIGINS = ["https://geode-solutions.com", "https://next.geode-solutions.com"]
+    MINUTES_BEFORE_TIMEOUT = "5"
+    SECONDS_BETWEEN_SHUTDOWNS = "45"
+
 
 class DevConfig(Config):
     SSL = None
-    ORIGINS = 'http://localhost:3000'
-    MINUTES_BEFORE_TIMEOUT = '1000'
-    SECONDS_BETWEEN_SHUTDOWNS = '60'
+    ORIGINS = "http://localhost:3000"
+    MINUTES_BEFORE_TIMEOUT = "1000"
+    SECONDS_BETWEEN_SHUTDOWNS = "60"
