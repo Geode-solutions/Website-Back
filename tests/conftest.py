9b00ea9f7f0db2c3211288b907874e9954f52906
JulienChampagnol <JulienChampagnol@users.noreply.github.com>
2023-10-11 13:59:44 +0000

    Apply prepare changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    app.config['SERVER_NAME'] = 'TEST'
-    app.config['DATA_FOLDER'] = './data/'
+    app.config["TESTING"] = True
+    app.config["SERVER_NAME"] = "TEST"
+    app.config["DATA_FOLDER"] = "./data/"
     client = app.test_client()
     yield client
