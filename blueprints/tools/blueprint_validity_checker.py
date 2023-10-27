6dbe1f500afdd3df507776dbf58592e083a5acd4
SpliiT <spliitdev@gmail.com>
2023-10-27 17:23:23 +0200

    update blueprint_validity_checker.py

--- blueprints/tools/blueprint_validity_checker.py
+++ blueprints/tools/blueprint_validity_checker.py
@@ -72,49 +72,51 @@ def validity_checker_test_names():
     model_checks = inspector_functions.json_return(
         inspector_functions.inspectors()[variables_dict["geode_object"]]["tests_names"]
     )
-    print(f"{model_checks=}")
+    # print(f"{model_checks=}")
     return flask.make_response({"model_checks": model_checks}, 200)
 
 
-@validity_checker_routes.route("/inspect_file", methods=["POST"])
+@validity_checker_routes.route("/inspect_file", methods=["POST", "OPTIONS"])
 def validity_checker_inspect_file():
-    UPLOAD_FOLDER = flask.current_app.config["UPLOAD_FOLDER"]
-    array_variables = ["geode_object", "filename", "test"]
-    variables_dict = geode_functions.get_form_variables(
-        flask.request.json, array_variables
-    )
+    if flask.request.method == "OPTIONS":
+        return flask.make_response({}, 200)
+    else:
+        UPLOAD_FOLDER = flask.current_app.config["UPLOAD_FOLDER"]
+        array_variables = ["geode_object", "filename", "test"]
+        variables_dict = geode_functions.get_form_variables(
+            flask.request.json, array_variables
+        )
 
-    secure_filename = werkzeug.utils.secure_filename(variables_dict["filename"])
-    file_path = os.path.abspath(os.path.join(UPLOAD_FOLDER, secure_filename))
-    data = geode_functions.load(variables_dict["geode_object"], file_path)
-    inspector = geode_functions.get_inspector(variables_dict["geode_object"], data)
-    test_result = getattr(inspector, variables_dict["test"])()
+        secure_filename = werkzeug.utils.secure_filename(variables_dict["filename"])
+        file_path = os.path.abspath(os.path.join(UPLOAD_FOLDER, secure_filename))
+        data = geode_functions.load(variables_dict["geode_object"], file_path)
+        inspector = geode_functions.get_inspector(variables_dict["geode_object"], data)
+        test_result = getattr(inspector, variables_dict["test"])()
 
-    if type(test_result) == int:
-        expected_result = 0
-    elif type(test_result) == list:
-        expected_result = []
-    elif type(test_result) == dict:
-        expected_result = {}
+        if type(test_result) == int:
+            expected_result = 0
+        elif type(test_result) == list:
+            expected_result = []
+        elif type(test_result) == dict:
+            expected_result = {}
 
-    if test_result != expected_result or type(test_result) != type(expected_result):
-        if type(test_result) is list:
-            if type(test_result[0]) is tuple:
-                temp_test_result = []
-                for tuple_item in test_result:
-                    temp_list = []
-                    for index in range(len(tuple_item)):
-                        temp_list.append(tuple_item[index].string())
-                    temp_test_result.append(temp_list)
-                test_result = temp_test_result
-    result = test_result == expected_result and type(test_result) == type(
-        expected_result
-    )
+        if test_result != expected_result or type(test_result) != type(expected_result):
+            if type(test_result) is list:
+                if type(test_result[0]) is tuple:
+                    temp_test_result = []
+                    for tuple_item in test_result:
+                        temp_list = []
+                        for index in range(len(tuple_item)):
+                            temp_list.append(tuple_item[index].string())
+                        temp_test_result.append(temp_list)
+                    test_result = temp_test_result
+        result = test_result == expected_result and type(test_result) == type(
+            expected_result
+        )
 
-    if result == False:
-        test_name = variables_dict["test"]
-        print(f"Wrong test result: {test_name}", flush=True)
-
-    return flask.make_response(
-        {"result": result, "list_invalidities": str(test_result)}, 200
-    )
+        if result == False:
+            test_name = variables_dict["test"]
+            print(f"Wrong test result: {test_name}", flush=True)
+        return flask.make_response(
+            {"result": result, "list_invalidities": str(test_result)}, 200
+        )
